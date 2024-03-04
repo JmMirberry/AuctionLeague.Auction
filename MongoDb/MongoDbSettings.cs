6c859ad1283debb486e560ce14b825e2934038e9
JMirberry <james.mirberry@podium-solutions.co.uk>
2024-03-04 16:37:54 +0000

    main Rename

--- MongoDb/MongoDbSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace AuctionLeague.MongoDb
-{
-    public class MongoDbSettings
-    {
-        public string ConnectionString { get; set; }
-
-        public string DatabaseName { get; set; }
-    }
-}
