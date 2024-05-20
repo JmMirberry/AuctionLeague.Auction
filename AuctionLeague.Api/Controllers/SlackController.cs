309bca27a83f6f16452c44dd60f0a166af0618c8
JmMirberry <53153211+JmMirberry@users.noreply.github.com>
2024-05-20 14:49:29 +0100

    Readd slash command handler

--- AuctionLeague.Api/Controllers/SlackController.cs
+++ AuctionLeague.Api/Controllers/SlackController.cs
@@ -12,16 +12,14 @@ namespace SlackAPI.Controllers
         private readonly ISlackRequestHandler _requestHandler;
         private readonly SlackEndpointConfiguration _endpointConfig;
         private readonly ISlackApiClient _slack;
-        //private readonly ISlashCommandHandler _commandHandler;
+        private readonly ISlashCommandHandler _commandHandler;
 
-        public SlackController(ISlackRequestHandler requestHandler, SlackEndpointConfiguration endpointConfig, ISlackApiClient slack
-            //, ISlashCommandHandler commandHandler
-            )
+        public SlackController(ISlackRequestHandler requestHandler, SlackEndpointConfiguration endpointConfig, ISlackApiClient slack, ISlashCommandHandler commandHandler)
         {
             _requestHandler = requestHandler;
             _endpointConfig = endpointConfig;
             _slack = slack;
-            //_commandHandler = commandHandler;
+            _commandHandler = commandHandler;
         }
 
         [HttpPost]
@@ -39,14 +37,14 @@ namespace SlackAPI.Controllers
              return await _requestHandler.HandleEventRequest(HttpContext.Request, _endpointConfig);
         }
 
-        //[HttpPost]
-        //[Route("[Controller]/Command")]
-        //public async Task<IActionResult> Command([FromBody] SlashCommand command)
-        //{
-        //    System.Diagnostics.Trace.TraceError("Log test");
-        //    var result = await _commandHandler.Handle(command);
-        //    return Ok(result);
-        //}
+        [HttpPost]
+        [Route("[Controller]/Command")]
+        public async Task<IActionResult> Command([FromBody] SlashCommand command)
+        {
+            System.Diagnostics.Trace.TraceError("Log test");
+            var result = await _commandHandler.Handle(command);
+            return Ok(result);
+        }
 
     }
 }
--- AuctionLeague.Api/Program.cs
+++ AuctionLeague.Api/Program.cs
@@ -40,8 +40,8 @@ builder.Services.AddSingleton(new SlackEndpointConfiguration().UseSigningSecret(
 
         builder.Services.AddSlackNet(c => c
             .UseApiToken(accessToken)
-            .RegisterEventHandler<MessageEvent, PingHandler>());
-            //.RegisterSlashCommandHandler<EchoDemo>());
+            .RegisterEventHandler<MessageEvent, PingHandler>()
+            .RegisterSlashCommandHandler<EchoDemo>(EchoDemo.SlashCommand));
 
         var app = builder.Build();
 
