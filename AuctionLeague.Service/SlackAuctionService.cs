baf5da664793bfb7cb7244a4c800a6cf786b8d7a
JmMirberry <53153211+JmMirberry@users.noreply.github.com>
2024-05-21 07:45:53 +0100

    Adding timer service

---
+++ AuctionLeague.Service/SlackAuctionService.cs
@@ -0,0 +1,28 @@
+﻿namespace AuctionLeague.Service;
+
+using Amazon.Runtime.Internal;
+using SlackNet;
+using SlackNet.AspNetCore;
+
+public class SlackAuctionService
+{ 
+    private readonly TimerService _timerService;
+    private readonly ISlackApiClient _slack;
+
+    public SlackAuctionService(TimerService timerService, ISlackApiClient slack)
+    {
+        _timerService = TimerService.Instance;
+        _timerService.TimerFinished += AuctionEnded();
+        _slack = slack;
+    }
+
+    public void StartAuction()
+    {
+        _timerService.StartTimer();
+    }
+
+    private Action AuctionEnded()
+    {
+        return _slack.Chat.PostMessage(new SlackNet.WebApi.Message() { Text = request.Message, Channel = request.SlackChannel }, null).Result();
+    }
+}
