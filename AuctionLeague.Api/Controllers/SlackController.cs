2d8acd0978dd86e0b555bbe8883ec1b957ce4714
JmMirberry <53153211+JmMirberry@users.noreply.github.com>
2024-05-29 08:55:32 +0100

    Update SlackController.cs

--- AuctionLeague.Api/Controllers/SlackController.cs
+++ AuctionLeague.Api/Controllers/SlackController.cs
@@ -1,9 +1,7 @@
-using Amazon.Runtime.Internal;
 using Microsoft.AspNetCore.Mvc;
 using SlackAPI.Models;
 using SlackNet;
 using SlackNet.AspNetCore;
-using SlackNet.Interaction;
 
 namespace AuctionLeague.Controllers
 {
@@ -13,16 +11,14 @@ namespace AuctionLeague.Controllers
         private readonly ISlackRequestHandler _requestHandler;
         private readonly SlackEndpointConfiguration _endpointConfig;
         private readonly ISlackApiClient _slack;
-        private readonly ISlashCommandHandler _commandHandler;
-
-        public SlackController(ISlackRequestHandler requestHandler, SlackEndpointConfiguration endpointConfig, ISlackApiClient slack, ISlashCommandHandler commandHandler)
+    
+        public SlackController(ISlackRequestHandler requestHandler, SlackEndpointConfiguration endpointConfig, ISlackApiClient slack)
         {
             _requestHandler = requestHandler;
             _endpointConfig = endpointConfig;
             _slack = slack;
-            _commandHandler = commandHandler;
         }
-
+    
         [HttpPost]
         [Route("[Controller]/Submit")]
         public async Task<ActionResult> Submit([FromBody] SlackMessage request)
@@ -30,30 +26,20 @@ namespace AuctionLeague.Controllers
             await _slack.Chat.PostMessage(new SlackNet.WebApi.Message() { Text = request.Message, Channel = request.SlackChannel }, null);
             return Ok();
         }
-
+    
         [HttpPost]
         [Route("[Controller]/Event")]
         public async Task<IActionResult> Event()
         {
              return await _requestHandler.HandleEventRequest(HttpContext.Request, _endpointConfig);
         }
-
+    
         [HttpPost]
         [Route("[Controller]/Command")]
         public async Task<IActionResult> Command()
         {
-            //Shouldn't need to do this, but can't get it to work
-            var command = new SlashCommand
-            {
-                Command = HttpContext.Request.Form["command"],
-                ChannelId = HttpContext.Request.Form["channel_id"],
-                ChannelName = HttpContext.Request.Form["channel_name"],
-                Text = HttpContext.Request.Form["text"]
-            };
-            
-            var result = await _commandHandler.Handle(command);
+            var result = await _requestHandler.HandleSlashCommandRequest(HttpContext.Request, _endpointConfig);
             return Ok(result);
         }
-
     }
 }
