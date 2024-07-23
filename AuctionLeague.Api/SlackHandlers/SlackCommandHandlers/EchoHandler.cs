30b62d275162fe453cddab7c2eeffbe1f9a50326
JmMirberry <53153211+JmMirberry@users.noreply.github.com>
2024-07-23 10:50:22 +0100

    Testing message sending

--- AuctionLeague.Api/SlackHandlers/SlackCommandHandlers/EchoHandler.cs
+++ AuctionLeague.Api/SlackHandlers/SlackCommandHandlers/EchoHandler.cs
@@ -1,4 +1,6 @@
-﻿using SlackNet;
+﻿using AuctionLeague.Data.Slack;
+using AuctionLeague.Service.DataStore;
+using SlackNet;
 using SlackNet.Events;
 using SlackNet.Interaction;
 using SlackNet.WebApi;
@@ -8,18 +10,36 @@ namespace AuctionLeague.Handlers.SlackCommandHandlers
     public class EchoDemo : ISlashCommandHandler
     {
         public const string SlashCommand = "/echo";
+        private readonly ISlackApiClient _slackClient;
 
-        public Task<SlashCommandResponse> Handle(SlashCommand command)
+        public EchoDemo(ISlackApiClient slackClient)
         {
+            _slackClient = slackClient;
+        }
+
+        public async Task<SlashCommandResponse> Handle(SlashCommand command)
+        {
+            await SendMessage(command.ChannelName, "This is a test message");
             Console.WriteLine($"{command.UserName} used the {SlashCommand} slash command in the {command.ChannelName} channel");
 
-            return Task.FromResult(new SlashCommandResponse
+            return new SlashCommandResponse
             {
                 Message = new Message
                 {
                     Text = command.Text
                 }
-            });
+            };
+        }
+
+        private async Task SendMessage(string channel, string message)
+        {
+            var slackMessage = new Message()
+            {
+                Text = message,
+                Channel = channel
+            };
+
+            await _slackClient.Chat.PostMessage(slackMessage, null);
         }
     }
 }
