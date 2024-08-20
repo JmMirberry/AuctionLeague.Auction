ece7f956f78e9679351c1fd54e592b9762a8ec39
JmMirberry <53153211+JmMirberry@users.noreply.github.com>
2024-08-20 14:32:32 +0100

    Update AutoNominationEntity.cs

--- AuctionLeague.MongoDb/Entities/AutoNominationEntity.cs
+++ AuctionLeague.MongoDb/Entities/AutoNominationEntity.cs
@@ -4,6 +4,7 @@ namespace AuctionLeague.MongoDb;
 
 public class AutoNominationEntity
 {
+    public string _id {  get; set; }
     public int Round { get; set; }
     public List<AutoNominatedPlayerEntity> Players { get; set; }
 }
