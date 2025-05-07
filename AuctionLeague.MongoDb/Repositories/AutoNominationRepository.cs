3a69caee9f60e83b9a009513c412396010d75f23
JmMirberry <53153211+JmMirberry@users.noreply.github.com>
2025-05-07 09:53:35 +0100

    fix

--- AuctionLeague.MongoDb/Repositories/AutoNominationRepository.cs
+++ AuctionLeague.MongoDb/Repositories/AutoNominationRepository.cs
@@ -25,7 +25,7 @@ namespace AuctionLeague.MongoDb.Repositories
         public async Task AddAutoNominationsAsync(IEnumerable<(int round, List<AuctionPlayer> players)> autoNominations) =>
             await _collection.InsertManyAsync(autoNominations.Select(e => new AutoNominationEntity
             {
-                _id = e.round
+                _id = e.round.ToString(),
                 Round = e.round,
                 Players = e.players.Select(x => x.ToAutoNominatedEntity()).ToList()
             }));
