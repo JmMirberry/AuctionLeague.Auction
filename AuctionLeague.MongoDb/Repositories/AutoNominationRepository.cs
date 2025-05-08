90b1428c8d43c9ee561499d51e160c855a659f15
JmMirberry <53153211+JmMirberry@users.noreply.github.com>
2025-05-08 11:04:35 +0100

    autonomination bug fix

--- AuctionLeague.MongoDb/Repositories/AutoNominationRepository.cs
+++ AuctionLeague.MongoDb/Repositories/AutoNominationRepository.cs
@@ -40,7 +40,7 @@ namespace AuctionLeague.MongoDb.Repositories
                 Builders<AutoNominationEntity>.Filter.Eq("Players.PlayerId", playerId)
                 );
 
-            var update = Builders<AutoNominationEntity>.Update.Set(p => p.Players[-1].Nominated, true);
+            var update = Builders<AutoNominationEntity>.Update.Set(p => p.Players[0].Nominated, true);
             await _collection.UpdateOneAsync(filter, update);
         }
     }
