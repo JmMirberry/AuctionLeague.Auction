bcc7644501024c44b6a98c2cfed0d7ffef74ad54
JmMirberry <53153211+JmMirberry@users.noreply.github.com>
2025-08-11 11:33:34 +0100

    Add player endpoint

--- AuctionLeague.Api/Controllers/AuctionTeamController.cs
+++ AuctionLeague.Api/Controllers/AuctionTeamController.cs
@@ -10,12 +10,14 @@ namespace AuctionLeague.Controllers
     public class AuctionTeamController : ControllerBase
     {
         private readonly IAuctionTeamRepository _auctionTeamsRepository;
+        private readonly IAuctionPlayerRepository _playerRepository;
         private readonly IPlayerSaleService _playerSaleService;
 
-        public AuctionTeamController(IAuctionTeamRepository auctionTeamsRepository, IPlayerSaleService playerSaleService)
+        public AuctionTeamController(IAuctionTeamRepository auctionTeamsRepository, IPlayerSaleService playerSaleService, IAuctionPlayerRepository playerRepository)
         {
             _auctionTeamsRepository = auctionTeamsRepository;
             _playerSaleService = playerSaleService;
+            _playerRepository = playerRepository;
         }
 
         [HttpGet]
@@ -45,10 +47,33 @@ namespace AuctionLeague.Controllers
         }
 
         [HttpPut]
-        [Route("add-player")]
-        public async Task<IActionResult> AddPlayer(string teamName, SoldPlayer player)
+        [Route("manually-add-player")]
+        public async Task<IActionResult> ManuallyAddPlayer(string teamName, SoldPlayer player)
         {
             var result = await _playerSaleService.ProcessSaleByTeamName(player, teamName);
+
+            return result.IsSuccess ? NoContent() : BadRequest(result.Errors);
+        }
+        
+        [HttpPut]
+        [Route("add-player/{auctionTeamName}/{playerId:int}/{salePrice:int}")]
+        public async Task<IActionResult> AddPlayer(string teamName, int playerId, int salePrice)
+        {
+            var player = await _playerRepository.GetPlayerAsync(playerId);
+
+            if (player == null )
+            {
+                return BadRequest($"No player found with id {playerId}");
+            }
+
+            if (player.IsSold)
+            {
+                return BadRequest($"Player is has already been sold");
+            }
+
+            var soldPlayer = new SoldPlayer(player, salePrice, DateTime.Now);
+            
+            var result = await _playerSaleService.ProcessSaleByTeamName(soldPlayer, teamName);
 
             return result.IsSuccess ? NoContent() : BadRequest(result.Errors);
         }
